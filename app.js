fc52f4fb16e52f5b4ab8dd26d41740c4949829fc
Mikey <79487718+mikeyxx@users.noreply.github.com>
2021-11-10 01:14:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,76 +22,92 @@ function changeTheme(e) {
     }
 }
 const calculator = document.querySelector('.calculator')
-const buttons = calculator.querySelectorAll('button');
 const display = calculator.querySelector('.calc-display');
+const keys = calculator.querySelector('.calc-keys');
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        const key = e.target;
-        const keyValue = key.textContent;
-        const displayValue = display.textContent;
-        const type = key.dataset.type;
-        const previousKeyType = calculator.dataset.previousKeyType;
-        //Is this a number key?
-//         if(type === 'number') {
-            if(displayValue === "0" || previousKeyType === 'operator') {
-                display.textContent = keyValue;
-            } else {
-                display.textContent = displayValue + keyValue;
-            }
-            const currentActiveOperator = calculator.querySelector('[data-state="selected"]')
-            if(currentActiveOperator) {
-                currentActiveOperator.dataset.state = "";
-            }
 
-        //}        
+keys.addEventListener('click', (e) => {
+    const key = e.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+    const type = key.dataset.type
+    const previousKeyType = calculator.dataset.previousKeyType
 
-        //Is this an operator key?
-        if(type === 'operator') {            
-            const currentActiveOperator = calculator.querySelector('[data-state="selected"]')
-            if(currentActiveOperator) {
-                currentActiveOperator.dataset.state = "";
-            }
-            key.dataset.state = 'selected'
-            //Save or get the firstNumber
-            calculator.dataset.firstNumber = displayValue;
-            //Also save the operator. The firstNum comes before an operator is clicked.
-            calculator.dataset.operator = key.dataset.key;
-        }
+if(type === 'number') {
+    if(displayValue === '0' || previousKeyType === 'operator') {
+        display.textContent = keyValue
+    } else {
+        display.textContent = displayValue + keyValue
+    }
+    const pressedOpKeys = keys.querySelectorAll('[data-type="operator"]')
+    pressedOpKeys.forEach(el => { el.dataset.state = '' })
+}
 
-        if(type === 'equal') {
-            //Perform a calculation
-            const firstNumber = calculator.dataset.firstNumber
-            const operator = calculator.dataset.operator;
-            const secondNumber = displayValue
-            display.textContent = calculate(firstNumber, operator, secondNumber)
-        }
+if(type === 'operator') { 
+    //To temporarily remove the state/classList from the operator key that was pressed
+    const pressedOpKeys = keys.querySelectorAll('[data-type="operator"]')
+    pressedOpKeys.forEach(el => { el.dataset.state = '' })
 
-        if(type === 'clear') {
-            //Reset the calculator
-            display.textContent = '0'
-            delete calculator.dataset.firstNumber
-            delete calculator.dataset.operator
-        }
+    //We need to save/detect the firstNumber
+    calculator.dataset.firstNumber = displayValue
 
-        if(type === 'delete') {
-            display.textContent = displayValue.slice(0, -1)
-        }
+    //We need to save/detect the operator key pressed
+    calculator.dataset.operator = key.dataset.key
 
-        calculator.dataset.previousKeyType = type;
-    })
+    //This adds something like a classList to the operator keys
+    key.dataset.state = 'selected'
+}
+
+if(type === 'equal') {
+    //perform a calculation
+    const firstNumber = calculator.dataset.firstNumber
+    const operator = calculator.dataset.operator
+    const secondNumber = displayValue
+    display.textContent = calculate(firstNumber, operator, secondNumber)
+    
+}
+
+if(type === 'delete') {
+    display.textContent = displayValue.slice(0, -1)
+}
+
+if(type === 'clear') {
+    display.textContent = '0'
+    delete calculator.dataset.firstNumber
+    delete calculator.dataset.operator
+    const pressedOpKeys = keys.querySelectorAll('[data-type="operator"]')
+    pressedOpKeys.forEach(el => { el.dataset.state = '' })
+}
+//To save/detect the previous key type that was pressed. We can do this by saving that calculator state somewhere. We can achieve that like this: const state {}. This lets us keep a state in a variable or we can just save the state in the html:
+calculator.dataset.previousKeyType = type
 })
 
-function calculate(firstNumber, operator, secondNumber) {
-    firstNumber = parseFloat(firstNumber)
-    secondNumber = parseFloat(secondNumber)
+let result = ''
+    function calculate(firstNumber, operator, secondNumber) {
+        if(operator === 'plus') {
+        result = parseFloat(firstNumber) + parseFloat(secondNumber)
+        } else if(operator === 'minus') {
+            result = parseFloat(firstNumber) - parseFloat(secondNumber)
+        } else if(operator === 'divide') {
+            result = parseFloat(firstNumber) / parseFloat(secondNumber)
+        } else if(operator === 'times') {
+            result = parseFloat(firstNumber) * parseFloat(secondNumber)
+        }
+        return result
+    }
 
-    if(operator === 'plus') { return firstNumber + secondNumber }
-    if(operator === 'minus') { return firstNumber - secondNumber }
-    if(operator === 'times') { return firstNumber * secondNumber }
-    if(operator === 'divide') { return firstNumber / secondNumber }
 
-    // let result = ''
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         const key = e.target;
+//         const keyValue = key.textContent;
+//         const displayValue = display.textContent;
+//         const type = type;
+//         const previousKeyType = calculator.dataset.previousKeyType;
+//         //Is this a number key?
+
+
+    // const result = ''
     // switch (operator) {
     //     case 'plus':
     //         result = firstNumber + secondNumber
@@ -107,6 +123,6 @@ function calculate(firstNumber, operator, secondNumber) {
     
     // return result
             
-}
+//}
 
 
